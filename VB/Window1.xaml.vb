5979f2bb7a8fcd9b2c75e0f80c809db7ef66c8e0
DevExpressExampleBot <fakeEmail>
2022-06-10 16:51:26 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,7 +1,7 @@
 Imports DevExpress.Xpf.Bars
 Imports DevExpress.Xpf.Grid
 Imports System
-Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
 Imports System.Windows
 
 Namespace DXGrid_CreateCellContextMenu
@@ -11,34 +11,35 @@ Namespace DXGrid_CreateCellContextMenu
 
         Public Sub New()
             Me.InitializeComponent()
-            Me.grid.ItemsSource = New AccountList().GetData()
+            Me.grid.ItemsSource = AccountList.GetData()
         End Sub
 
-'#Region "#RowCellMenuCustomization"
-        Private Sub copyCellDataItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
-            Dim menuInfo As GridCellMenuInfo = TryCast(Me.view.GridMenu.MenuInfo, GridCellMenuInfo)
-            If menuInfo IsNot Nothing AndAlso menuInfo.Row IsNot Nothing Then
-                Dim text As String = "" & Me.grid.GetCellValue(menuInfo.Row.RowHandle.Value, TryCast(menuInfo.Column, GridColumn)).ToString()
-                Clipboard.SetText(text)
+        Private Sub OnCopyRow(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            Dim menuInfo As GridCellMenuInfo = Nothing
+            If CSharpImpl.__Assign(menuInfo, TryCast(Me.view.GridMenu.MenuInfo, GridCellMenuInfo)) IsNot Nothing AndAlso menuInfo.Row IsNot Nothing Then
+                Me.grid.CopyCurrentItemToClipboard()
             End If
         End Sub
 
-        Private Sub deleteRowItem_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs)
-            Dim menuInfo As GridCellMenuInfo = TryCast(Me.view.GridMenu.MenuInfo, GridCellMenuInfo)
-            If menuInfo IsNot Nothing AndAlso menuInfo.Row IsNot Nothing Then Me.view.DeleteRow(menuInfo.Row.RowHandle.Value)
+        Private Sub OnDeleteRow(ByVal sender As Object, ByVal e As ItemClickEventArgs)
+            Dim menuInfo As GridCellMenuInfo = Nothing
+            If CSharpImpl.__Assign(menuInfo, TryCast(Me.view.GridMenu.MenuInfo, GridCellMenuInfo)) IsNot Nothing AndAlso menuInfo.Row IsNot Nothing Then Me.view.DeleteRow(menuInfo.Row.RowHandle.Value)
         End Sub
-'#End Region  ' #RowCellMenuCustomization
+
+        Private Class CSharpImpl
+
+            <Obsolete("Please refactor calling code to use normal Visual Basic assignment")>
+            Shared Function __Assign(Of T)(ByRef target As T, value As T) As T
+                target = value
+                Return value
+            End Function
+        End Class
     End Class
 
-'#Region "Account classes"
     Public Class AccountList
 
-        Public Function GetData() As List(Of Account)
-            Return CreateAccounts()
-        End Function
-
-        Private Function CreateAccounts() As List(Of Account)
-            Dim list As List(Of Account) = New List(Of Account)()
+        Public Shared Function GetData() As ObservableCollection(Of Account)
+            Dim list As ObservableCollection(Of Account) = New ObservableCollection(Of Account)()
             list.Add(New Account() With {.UserName = "Nick White", .CanBeDeleted = False, .RegistrationDate = Date.Today})
             list.Add(New Account() With {.UserName = "Jack Rodman", .CanBeDeleted = True, .RegistrationDate = New DateTime(2009, 5, 10)})
             list.Add(New Account() With {.UserName = "Sandra Sherry", .CanBeDeleted = False, .RegistrationDate = New DateTime(2008, 12, 22)})
@@ -56,5 +57,4 @@ Namespace DXGrid_CreateCellContextMenu
 
         Public Property CanBeDeleted As Boolean
     End Class
-'#End Region
 End Namespace
